ff49efdd29beeeb83366f9e875ccf23a4fe2a1c8
Kriszu <wangsizhu0504@outlook.com>
2023-08-04 08:49:35 +0800

    fix(checkbox): change trigger form validation

--- components/checkbox/Checkbox.tsx
+++ components/checkbox/Checkbox.tsx
@@ -61,6 +61,7 @@ export default defineComponent({
       const targetChecked = event.target.checked
       emit('update:checked', targetChecked)
       emit('change', event)
+      formItemContext.onFieldChange()
     }
     const checkboxRef = ref()
     const focus = () => {
