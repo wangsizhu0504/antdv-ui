a9c8f8719d1075b959b6b4c47394972ee7ea7550
Kriszu <wangsizhu0504@outlook.com>
2023-08-21 15:52:51 +0800

    feat: add px2rem in cssinjs

--- components/cssinjs/index.ts
+++ components/cssinjs/index.ts
@@ -1,10 +1,11 @@
 import useCacheToken from './hooks/useCacheToken'
 import useStyleRegister, { extractStyle } from './hooks/useStyleRegister'
 import Keyframes from './Keyframes'
-import { legacyNotSelectorLinter, logicalPropertiesLinter } from './linters/index'
+import { legacyNotSelectorLinter, logicalPropertiesLinter, parentSelectorLinter } from './linters/index'
 import { StyleProvider, createCache, useStyleInject, useStyleProvider } from './StyleContext'
 import { Theme, createTheme } from './theme/index'
 import legacyLogicalPropertiesTransformer from './transformers/legacyLogicalProperties'
+import px2remTransformer from './transformers/px2rem'
 import type { Linter } from './linters/interface'
 import type { StyleContextProps, StyleProviderProps } from './StyleContext'
 import type { DerivativeFunc, TokenType } from './theme/index'
@@ -24,10 +25,12 @@ const cssinjs = {
 
   // Transformer
   legacyLogicalPropertiesTransformer,
+  px2remTransformer,
 
   // Linters
   logicalPropertiesLinter,
   legacyNotSelectorLinter,
+  parentSelectorLinter,
 
   // cssinjs
   StyleProvider,
@@ -45,10 +48,12 @@ export {
 
   // Transformer
   legacyLogicalPropertiesTransformer,
+  px2remTransformer,
 
   // Linters
   logicalPropertiesLinter,
   legacyNotSelectorLinter,
+  parentSelectorLinter,
 
   // cssinjs
   StyleProvider,
