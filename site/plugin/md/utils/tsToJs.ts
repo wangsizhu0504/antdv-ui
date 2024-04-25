15e4a1157a085160f7ea5770e8cc05fb650d39ae
kriszu <39726513+wangsizhu0504@users.noreply.github.com>
2024-04-25 09:25:07 +0800

    fix: useForm model change validate error

--- site/plugin/md/utils/tsToJs.ts
+++ site/plugin/md/utils/tsToJs.ts
@@ -1,13 +1,15 @@
 import { transformSync } from '@babel/core'
-import { ESLint } from 'eslint'
-import transformTypescript from '@babel/plugin-transform-typescript'
-import baseConfig from '../../../../eslint.config.js'
 
-const engine = new ESLint({
-  fix: true,
-  useEslintrc: false,
-  baseConfig,
-})
+// import { ESLint } from 'eslint'
+import transformTypescript from '@babel/plugin-transform-typescript'
+
+// import baseConfig from '../../../../eslint.config.js'
+
+// const engine = new ESLint({
+//   fix: true,
+//   useEslintrc: false,
+//   baseConfig,
+// })
 async function tsToJs(content: string): Promise<string> {
   if (!content)
     return ''
@@ -23,10 +25,10 @@ async function tsToJs(content: string): Promise<string> {
       ],
     ],
   })
-  const report = await engine.lintText(code)
-  let output = report[0].output
-  output = output ? output.trim() : output
-  return output!
+  // const report = await engine.lintText(code)
+  // let output = report[0].output
+  // output = output ? output.trim() : output
+  return code!
 }
 
 export default tsToJs
