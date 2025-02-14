972d788fc72c6f297a5c7a8573513c5223f23222
kriszu <39726513+wangsizhu0504@users.noreply.github.com>
2025-02-14 17:36:53 +0800

    improve: build

--- build/src/tasks/types.ts
+++ build/src/tasks/types.ts
@@ -1,119 +1,25 @@
-import type { CompilerOptions, SourceFile } from 'ts-morph';
-import { mkdir, readFile, writeFile } from 'node:fs/promises';
+import { readFile, writeFile } from 'node:fs/promises';
 import path from 'node:path';
-import consola from 'consola';
 import glob from 'fast-glob';
-import pc from 'picocolors';
-import { Project } from 'ts-morph';
-import { antdRoot, buildOutput, pkgRoot, projRoot } from '../path';
-import { excludeFiles, pathRewriter } from '../utils';
+import { copy, remove } from 'fs-extra';
+import { buildOutput } from '../path';
+import { pathRewriter, run } from '../utils';
 
-const TSCONFIG_PATH = path.resolve(projRoot, 'tsconfig.web.json');
-const outDir = path.resolve(buildOutput, 'types');
-
-/**
- * fork = require( https://github.com/egoist/vue-dts-gen/blob/main/src/index.ts
- */
 export async function generateTypesDefinitions() {
-  const compilerOptions: CompilerOptions = {
-    declaration: true,
-    outDir,
-    skipDefaultLibCheck: true,
-    skipLibCheck: true,
-    emitDeclarationOnly: true,
-    preserveSymlinks: true,
-    noImplicitAny: false,
-  };
-  const project = new Project({
-    compilerOptions,
-    tsConfigFilePath: TSCONFIG_PATH,
-    skipAddingFilesFromTsConfig: true,
-  });
-
-  const sourceFiles = await addSourceFiles(project);
-  consola.success('Added source files');
-
-  typeCheck(project);
-  consola.success('Type check passed!');
-
-  await project.emit({
-    emitOnlyDtsFiles: true,
-  });
-
-  const tasks = sourceFiles.map(async (sourceFile) => {
-    const relativePath = path.relative(antdRoot, sourceFile.getFilePath());
-    consola.trace(
-      pc.yellow(
-        `Generating types file: ${pc.bold(relativePath)}`,
-      ),
-    );
-
-    const emitOutput = sourceFile.getEmitOutput();
-    const emitFiles = emitOutput.getOutputFiles();
-    if (emitFiles.length === 0)
-      throw new Error(`Emit no file: ${pc.bold(relativePath)}`);
-
-    const subTasks = emitFiles.map(async (outputFile) => {
-      const filepath = outputFile.getFilePath();
-      await mkdir(path.dirname(filepath), {
-        recursive: true,
-      });
-
-      await writeFile(
-        filepath,
-        pathRewriter('esm')(outputFile.getText()),
-        'utf8',
-      );
-    });
-    await Promise.all(subTasks);
-  });
-  consola.success(
-    pc.green(' Types file generated success'),
+  await run(
+    'npx vue-tsc -p tsconfig.web.json --declaration --emitDeclarationOnly --declarationDir dist/types',
   );
-  await Promise.all(tasks);
-}
-
-async function addSourceFiles(project: Project) {
-  project.addSourceFileAtPath(path.resolve(projRoot, 'typings/env.d.ts'));
-
-  const globSourceFile = '**/*.{js?(x),ts?(x)}';
-  const filePaths = excludeFiles(
-    await glob([globSourceFile, '!antdv-ui/**/*'], {
-      cwd: pkgRoot,
-      absolute: true,
-      onlyFiles: true,
-    }),
-  );
-  const antdPaths = excludeFiles(
-    await glob(globSourceFile, {
-      cwd: antdRoot,
-      onlyFiles: true,
-    }),
-  );
-
-  const sourceFiles: SourceFile[] = [];
-  await Promise.all([
-    ...filePaths.map(async (file) => {
-      const sourceFile = project.addSourceFileAtPath(file);
-      sourceFiles.push(sourceFile);
-    }),
-    ...antdPaths.map(async (file) => {
-      const content = await readFile(path.resolve(antdRoot, file), 'utf-8');
-      sourceFiles.push(
-        project.createSourceFile(path.resolve(pkgRoot, file), content),
-      );
-    }),
-  ]);
-
-  return sourceFiles;
-}
-
-function typeCheck(project: Project) {
-  const diagnostics = project.getPreEmitDiagnostics();
-  if (diagnostics.length > 0) {
-    consola.error(project.formatDiagnosticsWithColorAndContext(diagnostics));
-    const err = new Error('Failed to generate dts.');
-    consola.error(err);
-    throw err;
-  }
+  const typesDir = path.join(buildOutput, 'types', 'packages');
+  const filePaths = await glob(`**/*.d.ts`, {
+    cwd: typesDir,
+    absolute: true,
+  });
+  const rewriteTasks = filePaths.map(async (filePath) => {
+    const content = await readFile(filePath, 'utf8');
+    await writeFile(filePath, pathRewriter('esm')(content), 'utf8');
+  });
+  await Promise.all(rewriteTasks);
+  const sourceDir = path.join(typesDir, 'antdv-ui');
+  await copy(sourceDir, typesDir);
+  await remove(sourceDir);
 }
